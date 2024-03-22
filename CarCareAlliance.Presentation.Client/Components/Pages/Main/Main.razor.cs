2f8a02e10a6bed1ff931cf9047e8011e84abf21b
StrdL <stas20001234567@mail.ru>
2024-03-22 21:01:26 +0200

    Added service partners slider

---
+++ CarCareAlliance.Presentation.Client/Components/Pages/Main/Main.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace CarCareAlliance.Presentation.Client.Components.Pages.Main
+{
+	public partial class Main
+	{
+		private string searchString = string.Empty;
+
+		private void OnSearch(string text)
+		{
+			searchString = text;
+		}
+	}
+}
+
